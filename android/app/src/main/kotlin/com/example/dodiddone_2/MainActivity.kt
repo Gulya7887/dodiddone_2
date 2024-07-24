a9af84bf29860fe780b34746e92096e4f5c16150
Gulnora <gulnorakholbekova@gmail.com>
2024-07-24 21:47:19 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/dodiddone_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dodiddone_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:dodiddone_2/app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
